cc8c516a79b63a592c7fd7a46d3a05212b4f1e2f
yxyki <y1432621251@qq.com>
2022-04-24 15:38:37 +0000

    新增

---
+++ BubleSort.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+#define true 1
+#define false 0
+typedef int Bool;
+
+void LineModRandom(int n,int ranNum[]){
+    int i;
+    int max=2000;
+    srand((unsigned)(time(NULL)));
+    int m=rand()%(max+20);
+    int a=rand()%m-1;
+    int c=rand()%m-1;
+    ranNum[0]=rand()%(max+1)-1;
+    for(i=1;i<n;i++)
+      ranNum[i]=(a*ranNum[i-1]+c)%m;
+}
+
+void BubleSort(int List[],int n){
+  int numberOfPairs=n,i,temp;
+  Bool swappedElements=true;
+  while(swappedElements){
+      numberOfPairs--;
+      swappedElements=false;
+      for(i=0;i<numberOfPairs;i++){
+          if(List[i]>List[i+1]){
+              temp=List[i];
+              List[i]=List[i+1];
+              List[i+1]=temp;
+              swappedElements=true;
+          }
+      }
+  }
+}
+
+int main(){
+    int n,i;
+    printf("请输入排序的元素个数:");
+    scanf("%d",&n);
+    int x[n];
+    LineModRandom(n,x);
+    for(i=0;i<n;i++)
+      printf("%d ",x[i]);
+    BubleSort(x,n);
+    printf("\n排序之后为:\n");
+    for(i=0;i<n;i++)
+      printf("%d ",x[i]);
+}
