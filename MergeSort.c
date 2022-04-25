a48739534f6a13c71d11522b745add84ef5d038c
yxyki <y1432621251@qq.com>
2022-04-25 09:39:41 +0000

    新增

---
+++ MergeSort.c
@@ -0,0 +1,75 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int length;
+
+void MergeList(int List[],int start1,int end1,int start2,int end2){
+  int sorted[length];
+  int index=0,i;
+  int finalstart=start1,finalend=end2;
+  while(start1<=end1&&start2<=end2){
+      if(List[start1]<List[start2]){
+          sorted[index]=List[start1];
+          start1++;
+      }
+      else{
+          sorted[index]=List[start2];
+          start2++;
+      }
+      index++;
+  }
+  if(start1<=end1){
+      for(i=start1;i<=end1;i++){
+          sorted[index]=List[i];
+          index++;
+      }
+  }
+  else{
+      for(i=start2;i<=end2;i++){
+          sorted[index]=List[i];
+          index++;
+      }
+  }
+  index=0;
+  for(i=finalstart;i<=finalend;i++){
+      List[i]=sorted[index];
+      index++;
+  }
+}
+
+void LineModRandom(int n,int ranNum[]){
+    int i;
+    int max=2000;
+    srand((unsigned)(time(NULL)));
+    int m=rand()%(max+100);
+    int a=rand()%m-1;
+    int c=rand()%m-1;
+    ranNum[0]=rand()%(max+1)-1;
+    for(i=1;i<n;i++)
+      ranNum[i]=(a*ranNum[i-1]+c)%m;
+}
+
+void MergeSort(int List[],int first,int last){
+  if(first<last){
+      int mid=(last+first)/2;
+      MergeSort(List,first,mid);
+      MergeSort(List,mid+1,last);
+      MergeList(List,first,mid,mid+1,last);
+  }
+  else return;
+}
+
+int main(){
+    int i;
+    printf("请输入排序的元素个数:");
+    scanf("%d",&length);
+    int x[length];
+    LineModRandom(length,x);
+    for(i=0;i<length;i++)
+      printf("%d ",x[i]);
+    MergeSort(x,0,length);
+    printf("\n排序之后为:\n");
+    for(i=0;i<length;i++)
+      printf("%d ",x[i]);
+}
